0c27a6e98e74767ecbeef1d9eca375c131a28c12
TanmaySharmaG <tanmaysharmag.cy25@rvce.edu.in>
2026-06-02 01:37:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -82,4 +82,9 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
